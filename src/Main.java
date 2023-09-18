bcbb48cc293ddb691f918b508bea10a17489299e
SeveriusRasmussen <sora0001@stud.kea.dk>
2023-09-18 14:47:25 +0200

    New SuperHero

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        UserInterface ui = new UserInterface();
+        ui.StartProgram();
     }
 }
--- src/Superhero.java
+++ src/Superhero.java
@@ -1,2 +1,82 @@
-package PACKAGE_NAME;public class Superhero {
+//Attributes
+public class Superhero {
+    private String name;
+    private String realName;
+    private String superPower;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setRealName(String realName) {
+        this.realName = realName;
+    }
+
+    public void setSuperPower(String superPower) {
+        this.superPower = superPower;
+    }
+
+    public void setYearCreated(int yearCreated) {
+        this.yearCreated = yearCreated;
+    }
+
+    public void setIsHuman(String isHuman) {
+        this.isHuman = isHuman;
+    }
+
+    public void setStrenght(String strenght) {
+        this.strenght = strenght;
+    }
+
+    private int yearCreated;
+    private String isHuman;
+    private String strenght;
+
+    //konstruktør
+    public Superhero(String name, String realName,
+                     String superPower, int yearCreated, String isHuman, String strenght) {
+        this.name = name;
+        this.realName = realName;
+        this.superPower = superPower;
+        this.yearCreated = yearCreated;
+        this.isHuman = isHuman;
+        this.strenght = strenght;
+    }
+
+    //get metoder
+    public String getName() {
+        return name;
+    }
+
+    public String getRealName() {
+        return realName;
+    }
+
+    public String getSuperPower() {
+        return superPower;
+    }
+
+    public int getYearCreated() {
+        return yearCreated;
+    }
+
+    public String getIsHuman() {
+        return isHuman;
+    }
+
+    public String getStrenght() {
+        return strenght;
+    }
+
+
+
+    @Override
+    public String toString() {
+        return "\nSuperhero name: " + name +
+                "\nReal name: " + realName +
+                "\nSuper power: " + superPower +
+                " \nYear created: " + yearCreated +
+                " \nIs human: " + isHuman +
+                " \nStrenght: " + strenght;
+    }
 }
